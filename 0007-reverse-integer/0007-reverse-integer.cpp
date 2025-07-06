25741de3c7397089b2fa575b10b21848c0bf6b56
suman kumar <118544258+sumankumar2026@users.noreply.github.com>
2025-07-06 16:41:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (53.88%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,9 +4,9 @@ public:
         int reversed = 0;
         while(x != 0){
             int digit = x % 10;
-            if((reversed > INT_MAX / 10) || (reversed < INT_MIN / 10))  return 0;
+            if(reversed > INT_MAX / 10 || reversed < INT_MIN /10) return 0;
             reversed = reversed * 10 + digit;
-            x = x /10;
+            x = x / 10;
         }
         return reversed;
     }
